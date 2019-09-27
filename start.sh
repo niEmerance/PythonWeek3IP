f233270ec07e60cf68f53e5e673635d32ce6bd46
niEmerance <niyemera6@gmail.com>
2019-09-27 16:24:41 +0200

    Push on Heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # export MOVIE_API_KEY='58aed921d71acba6bbb12787dbef553c'
-export SECRET_KEY='<Flask WTF Secret Key>'
+export SECRET_KEY='emerance'
 export MAIL_USERNAME='niyemera6@gmail.com'
 export MAIL_PASSWORD='setako@kazuba'
 python3.6 manage.py server
